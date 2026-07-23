2bf54cf82632251a4ef6ce64a1d73d22b6775df0
Bishoy Adel <105159347+BishoyAdelAziz@users.noreply.github.com>
2026-07-23 17:18:30 +0300

    feat/ implment fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,31 +5,22 @@ import { SafeAreaView as RNFSafeAreaView } from "react-native-safe-area-context"
 const SafeAreaView = styled(RNFSafeAreaView);
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-start bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
+    <SafeAreaView className="flex-1 p-4  bg-background">
+      <Text className="text-7xl font-montserrat-semi-bold tracking-tighter ">
+        Home
       </Text>
+
       <Link
         href={"/onboadring"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-montserrat-bold-italic rounded bg-primary text-white p-4"
       >
         on boadring
       </Link>
       <Link
         href={"/(Auth)/sign-in/signin"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-montserrat-bold-italic rounded bg-primary text-white p-4"
       >
         sign in
-      </Link>
-      <Link
-        href={{ pathname: "/subscriptions/[id]", params: { id: "claude" } }}
-      >
-        Claude Max Subscription
-      </Link>
-      <Link
-        href={{ pathname: "/subscriptions/[id]", params: { id: "chatgpt" } }}
-      >
-        ChatGPT Plus Subscription
       </Link>
     </SafeAreaView>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,34 @@
 import "@/global.css";
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "montserrat-black": require("@/assets/fonts/Montserrat-Black.ttf"),
+    "montserrat-black-italic": require("@/assets/fonts/Montserrat-BlackItalic.ttf"),
+    "montserrat-bold": require("@/assets/fonts/Montserrat-Bold.ttf"),
+    "montserrat-bold-italic": require("@/assets/fonts/Montserrat-BoldItalic.ttf"),
+    "montserrat-extra-bold": require("@/assets/fonts/Montserrat-ExtraBold.ttf"),
+    "montserrat-extra-bold-italic": require("@/assets/fonts/Montserrat-ExtraBoldItalic.ttf"),
+    "montserrat-extra-light": require("@/assets/fonts/Montserrat-ExtraLight.ttf"),
+    "montserrat-extra-light-italic": require("@/assets/fonts/Montserrat-ExtraLightItalic.ttf"),
+    "montserrat-italic": require("@/assets/fonts/Montserrat-Italic.ttf"),
+    "montserrat-light": require("@/assets/fonts/Montserrat-Light.ttf"),
+    "montserrat-light-italic": require("@/assets/fonts/Montserrat-LightItalic.ttf"),
+    "montserrat-medium": require("@/assets/fonts/Montserrat-Medium.ttf"),
+    "montserrat-medium-italic": require("@/assets/fonts/Montserrat-MediumItalic.ttf"),
+    "montserrat-regular": require("@/assets/fonts/Montserrat-Regular.ttf"),
+    "montserrat-semi-bold": require("@/assets/fonts/Montserrat-SemiBold.ttf"),
+    "montserrat-semi-bold-italic": require("@/assets/fonts/Montserrat-SemiBoldItalic.ttf"),
+    "montserrat-thin": require("@/assets/fonts/Montserrat-Thin.ttf"),
+    "montserrat-thin-italic": require("@/assets/fonts/Montserrat-ThinItalic.ttf"),
+  });
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) return null;
   return <Stack screenOptions={{ headerShown: false }} />;
 }
